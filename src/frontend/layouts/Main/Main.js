7687b01e58893f218f4e29ab1d690b7a111c7b65
horsemaker <yash_ghodekar@yahoo.in>
2022-04-13 16:13:06 +0530

    add sign in & sign up routes

--- src/frontend/layouts/Main/Main.js
+++ src/frontend/layouts/Main/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "../../pages";
+import { HomePage, SignInPage, SignUpPage } from "../../pages";
 import "./Main.css";
 
 export const Main = () => {
@@ -8,6 +8,8 @@ export const Main = () => {
     <div className="main">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </div>
   );
