d6f7a588f5ec849f6de1ab5d9d81638c6d64a10f
horsemaker <yash_ghodekar@yahoo.in>
2022-04-13 16:12:05 +0530

    add auth constants

--- src/frontend/constants/index.js
+++ src/frontend/constants/index.js
@@ -1,2 +1,3 @@
 export { COKE_THEME } from "./theme-constants";
 export { LOGO_LIGHT, LOGO_DARK } from "./assets-constants";
+export { COKE_AUTH_USER_TOKEN, COKE_AUTH_USER_DATA } from "./auth-constants";
