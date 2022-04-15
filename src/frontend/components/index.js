6e23d9df29a41374509942ec8f0258c2b23cb01a
horsemaker <yash_ghodekar@yahoo.in>
2022-04-15 13:25:50 +0530

    add dropdown & sidebars

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -1,4 +1,7 @@
 export { Search } from "./Search/Search";
 export { ThemeToggle } from "./ThemeToggle/ThemeToggle";
+export { AccountDropdown } from "./AccountDropdown/AccountDropdown";
 export { ImageCarousel } from "./ImageCarousel/ImageCarousel";
 export { Banner } from "./Banner/Banner";
+export { ShrinkedSidebar } from "./ShrinkedSidebar/ShrinkedSidebar";
+export { ExpandedSidebar } from "./ExpandedSidebar/ExpandedSidebar";
