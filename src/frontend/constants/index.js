c1cd21c65237beaf842e31fb99d1ade921449b4e
horsemaker <yash_ghodekar@yahoo.in>
2022-04-17 00:34:06 +0530

    add filters constants

--- src/frontend/constants/index.js
+++ src/frontend/constants/index.js
@@ -1,3 +1,4 @@
 export { COKE_THEME } from "./theme-constants";
 export { LOGO_LIGHT, LOGO_DARK } from "./assets-constants";
 export { COKE_AUTH_USER_TOKEN, COKE_AUTH_USER_DATA } from "./auth-constants";
+export { CATEGORY } from "./filters-constants";
