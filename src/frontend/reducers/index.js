8dd2be2cb887a07de6ca1f9a5e409a217068fc0f
horsemaker <yash_ghodekar@yahoo.in>
2022-04-20 18:26:46 +0530

    add reducers

--- src/frontend/reducers/index.js
+++ src/frontend/reducers/index.js
@@ -1,1 +1,5 @@
 export { filtersReducer } from "./filtersReducer";
+export { likesReducer } from "./likesReducer";
+export { watchLaterReducer } from "./watchLaterReducer";
+export { historyReducer } from "./historyReducer";
+export { playlistsReducer } from "./playlistsReducer";
