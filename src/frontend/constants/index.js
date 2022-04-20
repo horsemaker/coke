a7172a449baebeb5c361c92f3b1ef0495e0fe042
horsemaker <yash_ghodekar@yahoo.in>
2022-04-20 18:23:13 +0530

    add constants

--- src/frontend/constants/index.js
+++ src/frontend/constants/index.js
@@ -2,3 +2,7 @@ export { COKE_THEME } from "./theme-constants";
 export { LOGO_LIGHT, LOGO_DARK } from "./assets-constants";
 export { COKE_AUTH_USER_TOKEN, COKE_AUTH_USER_DATA } from "./auth-constants";
 export { CATEGORY } from "./filters-constants";
+export { SET_LIKES } from "./likes-constants";
+export { SET_WATCH_LATER } from "./watch-later-constants";
+export { SET_HISTORY } from "./history-constants";
+export { SET_PLAYLISTS } from "./playlists-constants";
