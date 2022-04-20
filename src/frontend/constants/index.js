3be09a8c4e890590aacca8dfca3f9ef50ffdc389
horsemaker <yash_ghodekar@yahoo.in>
2022-04-21 01:59:57 +0530

    add empty state constant

--- src/frontend/constants/index.js
+++ src/frontend/constants/index.js
@@ -1,5 +1,5 @@
 export { COKE_THEME } from "./theme-constants";
-export { LOGO_LIGHT, LOGO_DARK } from "./assets-constants";
+export { LOGO_LIGHT, LOGO_DARK, EMPTY_STATE } from "./assets-constants";
 export { COKE_AUTH_USER_TOKEN, COKE_AUTH_USER_DATA } from "./auth-constants";
 export { CATEGORY } from "./filters-constants";
 export { SET_LIKES } from "./likes-constants";
