6019bf7413c30d3ab39ea3be5fa2536431fcafe7
horsemaker <yash_ghodekar@yahoo.in>
2022-04-21 02:02:24 +0530

    add watch later page

--- src/frontend/pages/WatchLaterPage/WatchLaterPage.js
+++ src/frontend/pages/WatchLaterPage/WatchLaterPage.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { EmptyStatePage } from "../EmptyStatePage/EmptyStatePage";
+import { BigHorizontalVideoCard } from "../../components";
 import { useWatchLater } from "../../contexts";
-import "./WatchLaterPage.css";
 
 export const WatchLaterPage = () => {
   const { watchLater } = useWatchLater();
 
-  return (
-    <div className="watch-later-page">
-      {watchLater.map((watchLaterVideo) => watchLaterVideo.title)}
+  return watchLater.length === 0 ? (
+    <EmptyStatePage />
+  ) : (
+    <div className="user-property-page">
+      <div className="user-property-page-cover">
+        <div className="user-property-page-cover-media">
+          <img
+            className="user-property-page-cover-img"
+            src={`https://img.youtube.com/vi/${watchLater[0]._id}/maxresdefault.jpg`}
+            alt="Watch Later Videos"
+          />
+          <div className="user-property-page-cover-title">
+            <span class="material-icons">watch_later</span>
+            <span>Watch Later</span>
+          </div>
+        </div>
+      </div>
+      <div className="user-property-page-videos">
+        {watchLater.map((watchLaterVideo) => (
+          <BigHorizontalVideoCard
+            key={watchLaterVideo._id}
+            video={watchLaterVideo}
+          />
+        ))}
+      </div>
     </div>
   );
 };
